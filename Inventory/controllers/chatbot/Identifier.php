257a7db228fd78a9257e29f56fb18b8508bb4d37
Paul Ian A. Dumdum <paulian.dumdum@gmail.com>
2026-03-13 14:23:39 +0800

    03132026

--- Inventory/controllers/chatbot/Identifier.php
+++ Inventory/controllers/chatbot/Identifier.php
@@ -4,6 +4,7 @@
             $input = str_replace(",","",$input);
             $input = str_replace(":","",$input);
             $models = [
+                "user" => new User(),
                 "ipaddress" => new IP_Address(),
                 "equipment" => new Equipment(),
                 "entry" => new Equipment_Entry(),
