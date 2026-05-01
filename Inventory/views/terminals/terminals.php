96a676bd29b3ca615c19ac42a48a9c622a0cfbdc
Paul Ian Dumdum <paulian.dumdum@gmail.com>
2026-05-01 14:42:24 +0800

    05012026 - Add work in progress.

--- Inventory/views/terminals/terminals.php
+++ Inventory/views/terminals/terminals.php
@@ -1,3 +1,5 @@
 <div class="terminals text-center text-secondary position-absolute" style="top: 50%; left: 50%; transform: translate(-10%,-50%);">
-    <h1>NOTHING IN HERE ...</h1>
+    <img class="wd-500" src="../../assets/img/work-in-progress.png" alt="" srcset="">
+    <h3>Under Construction</h3>
+    <h6>*Attention: Please wear your Personal Protective Equipment.</h6>
 </div>
