74b8dd4a4d234e14c85336ccee23d98d5b422155
Paul Ian Dumdum <paulian.dumdum@gmail.com>
2026-07-04 11:49:53 +0800

    07042026 - Test API

--- Inventory/controllers/api/test.php
+++ Inventory/controllers/api/test.php
@@ -1,11 +1,6 @@
 <?php
-    $allowed_origins = [];
-
-    $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-    if (in_array($origin, $allowed_origins)) {
-        header("Access-Control-Allow-Origin: $origin");
-    }
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true");
 
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
