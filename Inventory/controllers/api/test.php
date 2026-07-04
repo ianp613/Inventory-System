8bb5cee6d06e3f18c2ea6efaa142318580457c57
Paul Ian Dumdum <paulian.dumdum@gmail.com>
2026-07-04 11:52:30 +0800

    07042026 - Test API

--- Inventory/controllers/api/test.php
+++ Inventory/controllers/api/test.php
@@ -1,12 +1,17 @@
 <?php
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-    header("Access-Control-Allow-Credentials: true");
+    if(array_key_exists('HTTP_ORIGIN',$_SERVER)){
+        header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+        header("Access-Control-Allow-Credentials: true");
 
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        exit(0);
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            exit(0);
+        }
     }
+
+
+
 
     echo json_encode("Hello World");
