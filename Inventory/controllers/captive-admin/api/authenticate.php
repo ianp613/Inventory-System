8358e836679326d71f7d9d66787a5bf5fddfe1fc
Paul Ian Dumdum <paulian.dumdum@gmail.com>
2026-07-08 15:35:50 +0800

    07082026

--- Inventory/controllers/captive-admin/api/authenticate.php
+++ Inventory/controllers/captive-admin/api/authenticate.php
@@ -218,13 +218,11 @@ $newStatus = ($row["uses"] > 0 && $newUsesRemaining <= 0) ? "used" : $row["statu
 //     "status"         => $newStatus
 // ]);
 
-    $voucher_ = DB::find($voucher,"code","=",$input["code"]);
+$voucher_ = DB::find($voucher,"code","=",$input["code"]);
 
-    if(count($voucher_)){
-        $v = DB::prepare($voucher,$voucher_[0]["id"]);
-        $v->uses_remaining = (int)$voucher_[0]["uses_remaining"] - 1;
-        DB::update($v);
-    }
+    $v = DB::prepare($voucher,$voucher_[0]["id"]);
+    $v->uses_remaining = (int)$voucher_[0]["uses_remaining"] - 1;
+    DB::update($v);
 
 echo json_encode([
     "status"   => true,
